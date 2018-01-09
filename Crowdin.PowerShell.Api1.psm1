4b64e9d1b2ef9493d298c4e57267c9f3fe1f16d0
Yuriy Kushnir <yurko7@hotmail.com>
2018-01-09 23:04:00 +0200

    Add New-RequestBody cmdlet to build request body from cmdlet invocation.

--- Crowdin.PowerShell.Api1.psm1
+++ Crowdin.PowerShell.Api1.psm1
@@ -44,6 +44,46 @@ function Invoke-ApiRequest
     }
 }
 
+function New-RequestBody
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [System.Management.Automation.PSCmdlet]$Command,
+
+        [Parameter()]
+        [string[]]$ExcludeParameter
+    )
+
+    process {
+        $requestBody = [PSCustomObject]@{}
+        $myInvocation = $Command.MyInvocation
+        foreach ($boundParameterName in $myInvocation.BoundParameters.Keys)
+        {
+            if (($boundParameterName -in $ExcludeParameter) -or
+                ($boundParameterName -in [System.Management.Automation.Cmdlet]::CommonParameters) -or
+                ($boundParameterName -in [System.Management.Automation.Cmdlet]::OptionalCommonParameters))
+            {
+                continue
+            }
+            $parameter = $myInvocation.MyCommand.Parameters[$boundParameterName]
+            $parameterAliases = $parameter.Aliases
+            $requestParameterName = if ($parameterAliases) { $parameterAliases[0] } else { $boundParameterName }
+            $boundParameterValue = $myInvocation.BoundParameters[$boundParameterName]
+            if ($boundParameterValue -is [System.Management.Automation.SwitchParameter])
+            {
+                $boundParameterValue = ([int]$boundParameterValue.ToBool())
+            }
+            elseif ($boundParameterValue -is [System.DateTime])
+            {
+                $boundParameterValue = Get-Date -Date $boundParameterValue -Format s
+            }
+            $requestBody | Add-Member $requestParameterName.ToLower()  $boundParameterValue
+        }
+        $requestBody
+    }
+}
+
 function Test-Response
 {
     param (
