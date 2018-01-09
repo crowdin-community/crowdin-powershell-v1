2a285eac89380a7dc3253597484d5e33ea063f8d
Yuriy Kushnir <yurko7@hotmail.com>
2018-01-09 23:01:26 +0200

    Add process block.

--- Crowdin.PowerShell.Api1.psm1
+++ Crowdin.PowerShell.Api1.psm1
@@ -6,46 +6,48 @@ function Invoke-ApiRequest
     [CmdletBinding(DefaultParameterSetName='GET')]
     [OutputType([PSCustomObject])]
     param (
-        [Parameter(Mandatory=$true, Position=0)]
+        [Parameter(Mandatory, Position=0)]
         [uri]$Url,
 
-        [Parameter(Mandatory=$true, ValueFromPipeline=$true, ParameterSetName='POST')]
+        [Parameter(Mandatory, ValueFromPipeline, ParameterSetName='POST')]
         [psobject]$Body
     )
 
-    if ($PSCmdlet.ParameterSetName -eq 'GET')
-    {
-        $response = $HttpClient.GetAsync($Url).GetAwaiter().GetResult()
-    }
-    else
-    {
-        $content = New-Object System.Net.Http.MultipartFormDataContent
-        foreach ($member in ($Body | Get-Member -MemberType Properties))
+    process {
+        if ($PSCmdlet.ParameterSetName -eq 'GET')
         {
-            $partName = $member.Name
-            $partValue = $Body.($member.Name)
-            if ($partValue -is [System.IO.FileInfo])
-            {
-                $contentPart = New-Object System.Net.Http.StreamContent -ArgumentList ($partValue.OpenRead())
-                $content.Add($contentPart, $partName, $partValue.Name)
-            }
-            else
-            {
-                $contentPart = New-Object System.Net.Http.StringContent -ArgumentList $partValue
-                $content.Add($contentPart, $partName)
-            }
+            $response = $HttpClient.GetAsync($Url).GetAwaiter().GetResult()
         }
-        $response = $HttpClient.PostAsync($Url, $content).GetAwaiter().GetResult()
-        $content.Dispose()
+        else
+        {
+            $content = New-Object System.Net.Http.MultipartFormDataContent
+            foreach ($member in ($Body | Get-Member -MemberType Properties))
+            {
+                $partName = $member.Name
+                $partValue = $Body.($member.Name)
+                if ($partValue -is [System.IO.FileInfo])
+                {
+                    $contentPart = New-Object System.Net.Http.StreamContent -ArgumentList ($partValue.OpenRead())
+                    $content.Add($contentPart, $partName, $partValue.Name)
+                }
+                else
+                {
+                    $contentPart = New-Object System.Net.Http.StringContent -ArgumentList $partValue
+                    $content.Add($contentPart, $partName)
+                }
+            }
+            $response = $HttpClient.PostAsync($Url, $content).GetAwaiter().GetResult()
+            $content.Dispose()
+        }
+        $json = $response.Content.ReadAsStringAsync().GetAwaiter().GetResult()
+        ConvertFrom-Json -InputObject $json
     }
-    $json = $response.Content.ReadAsStringAsync().GetAwaiter().GetResult()
-    ConvertFrom-Json -InputObject $json
 }
 
 function Test-Response
 {
     param (
-        [Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true)]
+        [Parameter(Mandatory, Position=0, ValueFromPipeline)]
         $Response
     )
     process {
