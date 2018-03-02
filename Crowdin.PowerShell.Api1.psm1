92b7b59f048cde59a0ad0565a10f421e1c1f4eea
Yuriy Kushnir <yuriy.kushnir@crowdin.com>
2018-03-02 10:49:29 +0200

    Move function ConvertFrom-PSCmdlet to separate file.

--- Crowdin.PowerShell.Api1.psm1
+++ Crowdin.PowerShell.Api1.psm1
@@ -1,6 +1,7 @@
 New-Variable ApiBaseUrl -Value ([uri]'https://api.crowdin.com/api/') -Option Constant
 New-Variable HttpClient -Value (New-Object System.Net.Http.HttpClient -Property @{BaseAddress=$ApiBaseUrl}) -Option Constant
 
+. .\ConvertFrom-PSCmdlet.ps1
 function Invoke-ApiRequest
 {
     [CmdletBinding(DefaultParameterSetName='GET')]
@@ -41,46 +42,6 @@ function Invoke-ApiRequest
         }
         $json = $response.Content.ReadAsStringAsync().GetAwaiter().GetResult()
         ConvertFrom-Json -InputObject $json
-    }
-}
-
-function New-RequestBody
-{
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory, ValueFromPipeline)]
-        [System.Management.Automation.PSCmdlet]$Command,
-
-        [Parameter()]
-        [string[]]$ExcludeParameter
-    )
-
-    process {
-        $requestBody = [PSCustomObject]@{}
-        $myInvocation = $Command.MyInvocation
-        foreach ($boundParameterName in $myInvocation.BoundParameters.Keys)
-        {
-            if (($boundParameterName -in $ExcludeParameter) -or
-                ($boundParameterName -in [System.Management.Automation.Cmdlet]::CommonParameters) -or
-                ($boundParameterName -in [System.Management.Automation.Cmdlet]::OptionalCommonParameters))
-            {
-                continue
-            }
-            $parameter = $myInvocation.MyCommand.Parameters[$boundParameterName]
-            $parameterAliases = $parameter.Aliases
-            $requestParameterName = if ($parameterAliases) { $parameterAliases[0] } else { $boundParameterName }
-            $boundParameterValue = $myInvocation.BoundParameters[$boundParameterName]
-            if ($boundParameterValue -is [System.Management.Automation.SwitchParameter])
-            {
-                $boundParameterValue = ([int]$boundParameterValue.ToBool())
-            }
-            elseif ($boundParameterValue -is [System.DateTime])
-            {
-                $boundParameterValue = Get-Date -Date $boundParameterValue -Format s
-            }
-            $requestBody | Add-Member $requestParameterName.ToLower()  $boundParameterValue
-        }
-        $requestBody
     }
 }
 
