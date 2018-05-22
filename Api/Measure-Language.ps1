65dda99a3146b9b99385d4287ee747843d7b3355
Yuriy Kushnir <yuriy.kushnir@crowdin.com>
2018-05-22 16:10:51 +0300

    Add Measure-Project and Measure-Language cmdlets.

---
+++ Api/Measure-Language.ps1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+Language status.
+
+.DESCRIPTION
+Get the detailed translation progress for specified language.
+
+.PARAMETER ProjectId
+Should contain the project identifier.
+
+.PARAMETER ProjectKey
+Project API key.
+
+.PARAMETER Language
+Crowdin language code.
+
+.EXAMPLE
+An example
+
+.NOTES
+General notes
+#>
+function Measure-Language
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [Alias('identifier')]
+        [string]$ProjectId,
+
+        [Parameter(Mandatory)]
+        [Alias('key')]
+        [string]$ProjectKey,
+
+        [Parameter(Mandatory)]
+        [string]$Language
+    )
+
+    $ProjectId = [Uri]::EscapeDataString($ProjectId)
+    $body = $PSCmdlet | ConvertFrom-PSCmdlet -ExcludeParameter ProjectId
+    Invoke-ApiRequest -Url "project/$ProjectId/language-status?json" -Body $body | Test-Response
+}
---
+++ Api/Measure-Project.ps1
@@ -0,0 +1,33 @@
+<#
+.SYNOPSIS
+Translation status of the project.
+
+.DESCRIPTION
+Track overall translation and proofreading progresses of each target language.
+
+.PARAMETER ProjectId
+Should contain the project identifier.
+
+.PARAMETER ProjectKey
+Project API key.
+
+.EXAMPLE
+PS C:\> Measure-CrowdinProject -ProjectId apitestproject -ProjectKey 2b680...ce586
+#>
+function Measure-Project
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [Alias('identifier')]
+        [string]$ProjectId,
+
+        [Parameter(Mandatory)]
+        [Alias('key')]
+        [string]$ProjectKey
+    )
+
+    $ProjectId = [Uri]::EscapeDataString($ProjectId)
+    $body = $PSCmdlet | ConvertFrom-PSCmdlet -ExcludeParameter ProjectId
+    Invoke-ApiRequest -Url "project/$ProjectId/status?json" -Body $body | Test-Response
+}
