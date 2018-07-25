c38b890c8ed87aa0bf11fa07f9208d7004918631
Yuriy Kushnir <yuriy.kushnir@crowdin.com>
2018-07-25 14:54:01 +0300

    Add Add-TranslationMemory and Save-TranslationMemory cmdlets.

---
+++ Api/Save-TranslationMemory.ps1
@@ -0,0 +1,32 @@
+function Save-TranslationMemory
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$ProjectId,
+
+        [Parameter(Mandatory)]
+        [Alias('key')]
+        [string]$ProjectKey,
+
+        [Parameter()]
+        [Alias('include_assigned')]
+        [switch]$IncludeAssigned,
+
+        [Parameter()]
+        [Alias('source_language')]
+        [switch]$SourceLanguage,
+
+        [Parameter()]
+        [Alias('target_language')]
+        [switch]$TargetLanguage,
+
+        [Parameter()]
+        [string]$OutDir = (Get-Location)
+    )
+
+    $ProjectId = [Uri]::EscapeDataString($ProjectId)
+    $body = $PSCmdlet | ConvertFrom-PSCmdlet -ExcludeParameter ProjectId
+    $response = Invoke-ApiRequest -Url "project/$ProjectId/download-tm?json" -Body $body -OutDir $OutDir
+    $response.File
+}
