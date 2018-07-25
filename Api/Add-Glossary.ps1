59e622973c6927644b10d42568c633357cef54fb
Yuriy Kushnir <yuriy.kushnir@crowdin.com>
2018-07-25 13:11:33 +0300

    Add Add-Glossary and Save-Glossary cmdlets.

---
+++ Api/Add-Glossary.ps1
@@ -0,0 +1,29 @@
+function Add-Glossary
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [Alias('identifier')]
+        [string]$ProjectId,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [Alias('key')]
+        [string]$ProjectKey,
+
+        [Parameter(Mandatory)]
+        [System.IO.FileInfo]$File,
+
+        [Parameter()]
+        [Alias('first_line_contains_header')]
+        [switch]$FirstLineContainsHeader,
+
+        [Parameter()]
+        [string]$Scheme
+    )
+
+    $ProjectId = [Uri]::EscapeDataString($ProjectId)
+    $body = $PSCmdlet | ConvertFrom-PSCmdlet -ExcludeParameter ProjectId
+    Invoke-ApiRequest -Url "project/$ProjectId/upload-glossary?json" -Body $body
+}
---
+++ Api/Save-Glossary.ps1
@@ -0,0 +1,24 @@
+function Save-Glossary
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$ProjectId,
+
+        [Parameter(Mandatory)]
+        [Alias('key')]
+        [string]$ProjectKey,
+
+        [Parameter()]
+        [Alias('include_assigned')]
+        [switch]$IncludeAssigned,
+
+        [Parameter()]
+        [string]$OutDir = (Get-Location)
+    )
+
+    $ProjectId = [Uri]::EscapeDataString($ProjectId)
+    $body = $PSCmdlet | ConvertFrom-PSCmdlet -ExcludeParameter ProjectId
+    $response = Invoke-ApiRequest -Url "project/$ProjectId/download-glossary?json" -Body $body -OutDir $OutDir
+    $response.File
+}
