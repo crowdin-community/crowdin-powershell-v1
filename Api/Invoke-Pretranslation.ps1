914acebc8094912ddb7c2ccdb2c213f2aa31e23d
Yuriy Kushnir <yuriy.kushnir@crowdin.com>
2018-07-25 19:26:03 +0300

    Add Invoke-Pretranslation cmdlet.

---
+++ Api/Invoke-Pretranslation.ps1
@@ -0,0 +1,55 @@
+function Invoke-Pretranslation
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [Alias('identifier')]
+        [string]$ProjectId,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [Alias('key')]
+        [string]$ProjectKey,
+
+        [Parameter(Mandatory)]
+        [Alias('languages')]
+        [string[]]$Language,
+
+        [Parameter(Mandatory)]
+        [Alias('files')]
+        [string[]]$File,
+
+        [Parameter()]
+        [ValidateSet('tm', 'mt')]
+        [string]$Method,
+
+        [Parameter()]
+        [string]$Engine,
+
+        [Parameter()]
+        [Alias('approve_translated')]
+        [switch]$ApproveTranslated,
+
+        [Parameter()]
+        [ValidateRange(0, 2)]
+        [Alias('auto_approve_option')]
+        [int]$AutoApproveOption,
+
+        [Parameter()]
+        [Alias('import_duplicates')]
+        [switch]$ImportDuplicates,
+
+        [Parameter()]
+        [Alias('apply_untranslated_strings_only')]
+        [switch]$UntranslatedStringsOnly,
+
+        [Parameter()]
+        [Alias('perfect_match')]
+        [switch]$PerfectMatch
+    )
+
+    $ProjectId = [Uri]::EscapeDataString($ProjectId)
+    $body = $PSCmdlet | ConvertFrom-PSCmdlet -ExcludeParameter ProjectId
+    Invoke-ApiRequest -Url "project/$ProjectId/pre-translate?json" -Body $body
+}
